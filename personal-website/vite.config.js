086e35d63531a715796944290e3ccda12a1ebc2c
udbhavbalaji <udbhavbalaji@gmail.com>
2024-09-02 21:09:18 -0400

    Configured paths for easier readability and resolved module in config files

--- personal-website/vite.config.js
+++ personal-website/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@data": path.resolve(__dirname, "src/data"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+    },
+  },
+});
